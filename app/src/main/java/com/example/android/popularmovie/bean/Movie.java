5d31eb6143cd39c35b651b04226aadb2c86b8623
misswangquedong <shardpreference@gmail.com>
2017-01-09 20:46:09 +0800

    修改为继承Parcelable类效率更高

--- app/src/main/java/com/example/android/popularmovie/bean/Movie.java
+++ app/src/main/java/com/example/android/popularmovie/bean/Movie.java
@@ -1,86 +1,86 @@
 package com.example.android.popularmovie.bean;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by Administrator on 2017/1/6.
  */
 
-public class Movie  implements Serializable {
+public class Movie implements Parcelable {
     private String title;//片名
     private String poster;//ctrl+shift+u大小写转换
     private String overview;//概观
-    private Double vote_average;//平均投票
+    private double vote_average;//平均投票
     private String release_date;//上映日期
-    private Double popularity;//最受欢迎
+    private double popularity;//最受欢迎
+
+    public Movie(String title, String poster, String overview, double vote_average, String release_date, double popularity) {
+        this.title = title;
+        this.poster = poster;
+        this.overview = overview;
+        this.vote_average = vote_average;
+        this.release_date = release_date;
+        this.popularity = popularity;
+    }
 
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getPoster() {
         return poster;
     }
 
-    public void setPoster(String poster) {
-        this.poster = poster;
-    }
-
     public String getOverview() {
         return overview;
     }
 
-    public void setOverview(String overview) {
-        this.overview = overview;
-    }
-
-    public Double getVote_average() {
+    public double getVote_average() {
         return vote_average;
-    }
-
-    public void setVote_average(Double vote_average) {
-        this.vote_average = vote_average;
     }
 
     public String getRelease_date() {
         return release_date;
     }
 
-    public void setRelease_date(String release_date) {
-        this.release_date = release_date;
-    }
-
-    public Double getPopularity() {
+    public double getPopularity() {
         return popularity;
     }
 
-    public void setPopularity(Double popularity) {
-        this.popularity = popularity;
+    protected Movie(Parcel in) {
+        title = in.readString();
+        poster = in.readString();
+        overview = in.readString();
+        vote_average = in.readDouble();
+        release_date = in.readString();
+        popularity = in.readDouble();
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     @Override
-    public String toString() {
-        return "Movie{" +
-                "title='" + title + '\'' +
-                ", poster='" + poster + '\'' +
-                ", overview='" + overview + '\'' +
-                ", vote_average=" + vote_average +
-                ", release_date='" + release_date + '\'' +
-                ", popularity=" + popularity +
-                '}';
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(poster);
+        dest.writeString(overview);
+        dest.writeDouble(vote_average);
+        dest.writeString(release_date);
+        dest.writeDouble(popularity);
     }
-    /* @Override
-    public int compareTo(Movie movie) {
-        if(this.popularity>movie.popularity){
-            return 1 ;
-        }else if(this.popularity<movie.popularity){
-            return -1 ;
-        }else{
-            return this.popularity.compareTo(movie.popularity) ;	// 调用String中的compareTo()方法
-        }
-    }*/
 }
